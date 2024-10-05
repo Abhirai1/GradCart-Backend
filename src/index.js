d3bb52b33d5109457e94ac69804cea541a93a37f
Abhishek Rai <abhirai2250@gmail.com>
2024-10-05 11:46:37 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// require('dotenv').config({path: './env'});
+// ye ek tarka hai
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("connection failed! ", err);
+  });
